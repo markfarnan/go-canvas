8f40d56069fd3ad55c8632e6705494e70a107d09
Mark Farnan <mark@markfarnan.com>
2019-05-05 17:03:58 +0200

    Fix copyright

--- demo/main.go
+++ demo/main.go
@@ -1,3 +1,20 @@
+// Copyright [2019] [Mark Farnan]
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+// NOTICE:  Much of this demo is a re-write of the 'Moving red Laser' demo by Martin Olsansky https://medium.freecodecamp.org/webassembly-with-golang-is-fun-b243c0e34f02
+// It has been re-written to make use of the go-canvas library,  and avoid context calls for drawing.
+
 package main
 
 import (
